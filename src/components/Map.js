a86b69ecaf652a90a3e49d97587ac67d31db173d
jairo8925 <csarmiento2333@gmail.com>
2021-07-12 20:35:50 -0700

    added bounds to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,27 @@
 import './Map.css';
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 
 class Map extends React.Component {
   render() {
+    const corner1 = L.latLng(-90, -200);
+    const corner2 = L.latLng(90, 200);
+    const bounds = L.latLngBounds(corner1, corner2);
+
     return (
-      <MapContainer center={[0, 0]} zoom={2}>
+      <MapContainer
+        center={[5, 0]}
+        zoom={2}
+        doubleClickZoom={false}
+        touchZoom={false}
+        zoomSnap={false}
+        zoomDelta={false}
+        trackResize={false}
+        scrollWheelZoom={false}
+        maxBoundsViscosity={1.0}
+        maxBounds={bounds}
+      >
         <TileLayer
           url="http://tile.stamen.com/terrain-background/{z}/{x}/{y}.jpg"
           attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, 
@@ -16,6 +32,7 @@ class Map extends React.Component {
         <Marker
           position={[Number(this.props.lat), Number(this.props.long)]}
           keepInView={true}
+          autoPan={true}
         />
       </MapContainer>
     );
